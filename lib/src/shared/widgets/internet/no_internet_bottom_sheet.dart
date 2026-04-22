4da4843a98e2aa145a27b7b829476ad441dc635a
mi.kazemi <mi.kazemi@behpardaz.net>
2026-04-22 11:20:18 +0330

    fix: remove useless message from no internet bottom sheet widget

--- lib/src/shared/widgets/internet/no_internet_bottom_sheet.dart
+++ lib/src/shared/widgets/internet/no_internet_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:eks_sana_plus_org/src/shared/resources/assets_manager.dart';
 import 'package:eks_sana_plus_org/src/shared/resources/value_manager.dart';
 import 'package:eks_sana_plus_org/src/shared/widgets/inkwell_button_widget/inkwell_button_widget.dart';
+import 'package:eks_sana_plus_org/src/shared/widgets/text_widgets/body_medium_text.dart';
 import 'package:flutter/material.dart';
 
 class NoInternetBottomSheet extends StatelessWidget {
@@ -41,24 +42,9 @@ class NoInternetBottomSheet extends StatelessWidget {
   }
 
   Widget _buildSubtitle(BuildContext context) {
-    final theme = Theme.of(context).textTheme.bodyMedium;
-
-    return RichText(
-      textAlign: TextAlign.start,
-      text: TextSpan(
-        style: theme,
-        children: [
-          const TextSpan(
-            text:
-                'اتصال به اینترنت را بررسی و دوباره تلاش کنید.\nدر صورت دسترسی نداشتن به اینترنت، ',
-          ),
-          TextSpan(
-            text: 'برای درخواست تلفنی امداد با 096550',
-            style: theme?.copyWith(fontWeight: FontWeight.bold),
-          ),
-          const TextSpan(text: ' تماس بگیرید.'),
-        ],
-      ),
+    return const BodyMediumText(
+      text:
+          'اتصال به اینترنت را بررسی و دوباره تلاش کنید.\nدر صورت دسترسی نداشتن به اینترنت، ',
     );
   }
 
