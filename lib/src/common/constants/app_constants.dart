ea8b1c6bbf37f8ccbf7fe914fb5a712fee9f87e6
mi.kazemi <mi.kazemi@behpardaz.net>
2026-04-27 13:07:46 +0330

    refactor: impl pagination for relief request list
    # Conflicts:
    #	lib/src/common/constants/app_constants.dart
    #	lib/src/features/services/presentation/relief_request_list_page/cubit/relief_request_list_cubit.dart

--- lib/src/common/constants/app_constants.dart
+++ lib/src/common/constants/app_constants.dart
@@ -24,10 +24,14 @@ class AppConstants {
   static const String baseSocketUrl = "ws://emdadapi.emdadsaipa.ir/api/Notification/Connect/";
   static const String appImagesUrl = "https://emdadapi.emdadsaipa.ir/content/mobile-app-images/";
 
+  ///test server
+  //static const String baseApiUrl = "http://172.16.4.97:11002";
+
   ///amir system test server
-  static const String baseApiUrl = "http://172.16.170.65:11002";
+  //static const String baseApiUrl = "http://172.16.170.65:11002";
 
-
+  ///mohammad system test server
+  static const String baseApiUrl = "http://172.16.170.51:11002";
 
    //static const String baseApiUrl = "http://172.16.4.97:8012";
   // static const String baseApiUrl = "http://172.16.4.97:5000";
